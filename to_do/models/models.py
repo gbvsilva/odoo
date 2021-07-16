09b39761f2d106e292005298be8b44a1cc55665b
gbvsilva <graco.babeuf18@gmail.com>
2021-07-16 19:09:09 -0300

    Update models and views - to_do

--- to_do/models/models.py
+++ to_do/models/models.py
@@ -7,11 +7,11 @@ class to_do(models.Model):
     _name = 'to_do.to_do'
     _description = 'to_do.to_do'
 
-    name = fields.Char(required=True)
-    author = fields.Char(required=True)
+    name = fields.Char(required=True, string='Title')
+    author = fields.Char(required=True, string='Author')
     partner_id = fields.Selection(selection=[('primario', 'Primario'), 
                 ('secundario', 'Secundario')], string='Funcao', default='primario')
-    description = fields.Text()
-    isFinished = fields.Boolean()
-    createdAt = fields.Date(required=True)
+    description = fields.Text(string='Description')
+    isFinished = fields.Boolean(string='isFinished')
+    createdAt = fields.Date(required=True, string='createdAt')
     
